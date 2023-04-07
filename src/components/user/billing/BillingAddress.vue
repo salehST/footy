d66a7c20c08edaa015cafbccbb54aca91786630f
CoderSaleh <codersaleh@gmail.com>
2023-04-07 19:36:04 +0600

    address form

--- src/components/user/billing/BillingAddress.vue
+++ src/components/user/billing/BillingAddress.vue
@@ -6,7 +6,7 @@
 				<button @click="editTrue(1)" type="button" :class="{ 'hidden': showForm === 1, 'block': showForm !== 1 }" class="pane__edit"> Edit </button>
 				<div :class="{ 'hidden': showForm === 1, 'block': showForm !== 1 }" id="account_address" class="text-black text-14 md:text-18 font-500 w-3/4 md:w-1/3">Sportpark Spieringhorn 10
 1043 AA, Amsterdam, Netherlands</div>
-				<div  v-show="showForm === 1" class="update-address">
+				<div :class="{ 'block': showForm === 1, 'hidden': showForm !== 1 }" class="update-address">
 					<div class="form-area">
 						<input type="text" name="name" class=" text-grey-dark text-14 font-500 py-2 px-3 border border-gray-500 hover:outline-none focus:outline-none hover:border-gray-600 transition focus:ring-0 focus:border-gray-500 mb-1"  id="name" placeholder="Naam op factuur" >
 						<input type="text" name="street" class=" text-grey-dark text-14 font-500 py-2 px-3 border border-gray-500 hover:outline-none focus:outline-none hover:border-gray-600 transition focus:ring-0 focus:border-gray-500 mb-1"  id="street" placeholder="Straatnaam en nummer" >
